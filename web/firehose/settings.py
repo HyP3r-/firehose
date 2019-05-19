4d9e74abe2362ea53982bc5e7627c4a2a81dfd66
Andreas Fendt <mail@andreas-fendt.de>
2019-05-19 22:25:13 +0200

    settings.py: added app models.py: created basic model for this project

--- web/firehose/settings.py
+++ web/firehose/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "hose.apps.HoseConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- web/hose/models.py
+++ web/hose/models.py
@@ -1,1 +1,52 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class HoseType(models.Model):
+    """
+    Different Hose Types like A, B or C
+    """
+
+    name = models.CharField(unique=True, max_length=250)
+
+
+class HoseManufacturer(models.Model):
+    """
+    Manufacturer of Hoses
+    """
+
+    name = models.CharField(unique=True, max_length=250)
+
+
+class HoseEvent(models.Model):
+    """
+    Events of Hoses
+    """
+
+    name = models.CharField(unique=True, max_length=250)
+
+
+class Hose(models.Model):
+    """
+    List of Hoses
+    """
+
+    barcode = models.CharField(unique=True, null=True, max_length=250)
+    build_year = models.IntegerField(null=True)
+    description = models.CharField(blank=True, max_length=250)
+    hose_type = models.ForeignKey(HoseType, on_delete=models.CASCADE)
+    length = models.IntegerField()
+    manufacturer = models.ForeignKey(HoseManufacturer, on_delete=models.CASCADE)
+    number = models.IntegerField(unique=True)
+
+
+class HoseHistory(models.Model):
+    """
+    History of a Hose
+    """
+
+    date = models.DateTimeField()
+    description = models.CharField(blank=True, max_length=250)
+    event = models.ForeignKey(HoseEvent, on_delete=models.CASCADE)
+    hose = models.ForeignKey(Hose, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
