0a7a3c0583562594fd2c811b25c48a5c3433b50f
Andreas Fendt <mail@andreas-fendt.de>
2019-05-22 02:02:58 +0200

    global.js: refactored

--- web/hose/static/hose/js/global.js
+++ web/hose/static/hose/js/global.js
@@ -1,13 +1,33 @@
+/**
+ * read csrf token
+ */
+var csrftoken = Cookies.get("csrftoken");
+
+/**
+ * check if request has to send an csrf token
+ */
 function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 
+/**
+ * configure ajax
+ */
 $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
+    beforeSend: function (xhr, settings) {
         if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            var csrftoken = Cookies.get('csrftoken');
             xhr.setRequestHeader("X-CSRFToken", csrftoken);
         }
     }
 });
+
+/**
+ * bind jquery ajax request to pace
+ */
+window.paceOptions = {
+    restartOnRequestAfter: 50,
+    ajax: {
+        trackMethods: ["GET", "POST"]
+    }
+};
